cc6cd80c05ffd3dc4c4e2ac56808c20528e9f0b8
Himanshu Mishra <mishrah225@gmail.com>
2018-10-23 13:39:09 +0530

    Update rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -18,9 +18,9 @@ bool isPrime(long long n)
 	for(i=2; i * i <= n; i++)
 	{
 		if(n % i==0)
-        {
-            return 0;
-        }
+        	{
+            		return 0;
+        	}
 	}
 	return 1;
 }
